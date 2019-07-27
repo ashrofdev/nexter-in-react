789e71ffe4f46eeef824dc818feccc85bedf9e1a
First Last <salmanashraf853@gmail.com>
2019-07-27 07:13:23 +0100

    creating structure

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Story from './components/Story';
 import Cards from './components/Cards';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
+import SideBar from './components/SideBar';
 
 
 function App() {
   return (
     <div className="App">
       <Head/>
+      <SideBar/>
       <Features/>
       <Story/>
       <Cards/>
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <div className="side-bar">
+      {'SideBar section'}
+    </div>
+  );
+}
+
+export default SideBar;
