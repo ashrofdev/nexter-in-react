0ec524c0a4e71a1cc00fa92e1129e499024ce622
First Last <salmanashraf853@gmail.com>
2019-07-29 17:19:36 +0100

    finishing the text side

--- src/components/Story.js
+++ src/components/Story.js
@@ -7,8 +7,11 @@ const Story = () => {
         {'gallery'}
       </div>
       <div className="right">
-        {'text'}
+        <p>HAPPY CUSTOMERS</p>
         <h1>“ The best decision of our lives”</h1>
+        <p className="story">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+         distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta ! </p>
+         <button>FIND YOUR OWN HOME</button>
       </div>
     </div>
   );
