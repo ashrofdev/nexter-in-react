c64927d6865af1d588d78aaa9e405e3d2305cc5c
First Last <salmanashraf853@gmail.com>
2019-07-29 13:43:50 +0100

    starting with the feature section

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,7 +3,35 @@ import React from 'react';
 const Features = () => {
   return (
     <div className="features">
-      {'Features section'}
+      <div className="feature">
+        <h2>World's best luxury homes</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Tenetur distinctio necessitatibus pariatur voluptatibus. </p>
+      </div>
+      <div className="feature">
+        <h2>Only the best properties</h2>
+        <p>Voluptatum mollitia quae. Vero ipsum sapiente molestias
+         accusamus rerum sed a eligendi aut quia.  </p>
+      </div>
+      <div className="feature">
+        <h2>All homes in in top locations</h2>
+        <p>Tenetur distinctio necessitatibus pariatur voluptatibus
+          quidem consequatur harum. </p>
+      </div>
+      <div className="feature">
+        <h2>New home in one week</h2>
+        <p>Vero ipsum sapiente molestias accusamus rerum. Lorem, i
+        psum dolor sit amet consectetur adipisicing elit.  </p>
+      </div>
+      <div className="feature">
+        <h2>Top 1% realtors</h2>
+        <p>Quidem consequatur harum, voluptatum mollitia quae. Tenetur
+         distinctio necessitatibus pariatur voluptatibus. </p>
+      </div>
+      <div className="feature">
+        <h2>Secure payment on nexter</h2>
+        <p>Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae. </p>
+      </div>
     </div>
   );
 }
