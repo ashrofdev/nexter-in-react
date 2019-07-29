d253dd727f9eda4768c932e7c7c6fc22f42ddaae
First Last <salmanashraf853@gmail.com>
2019-07-29 15:15:16 +0100

    building the next section

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SideBar = () => {
   return (
     <div className="side-bar">
-      {'SideBar section'}
+      {'SideBar'}
     </div>
   );
 }
--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Story = () => {
   return (
     <div className="story">
-      {'Story section'}
+      <div className="left">
+        {'gallery'}
+      </div>
+      <div className="right">
+        {'text'}
+        <h1>“ The best decision of our lives”</h1>
+      </div>
     </div>
   );
 }
