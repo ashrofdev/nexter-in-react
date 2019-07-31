2cf1ee073f4579eedd5afbe19f607c64ae3c37da
First Last <salmanashraf853@gmail.com>
2019-07-31 15:09:50 +0100

    improving the header

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,6 +5,12 @@ const Head = () => {
     <div className="head">
       <div className="main">
         main header section
+        <img className="logo" alt="" src={require('./img/logo.png')} />
+        <div className="heading">
+          <p>YOUR OWN HOME</p>
+          <h1>The ultimate personal freeedom</h1>
+          <button>FIND YOUR OWN HOME</button>
+        </div>
       </div>
       <div className="realtor">
         realtor section
