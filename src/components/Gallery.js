766406355ffcaaf0819c0f81af38f3478eddf014
First Last <salmanashraf853@gmail.com>
2019-08-02 11:25:22 +0100

    building gallery section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,6 +3,48 @@ import React from 'react';
 const Gallery = () => {
   return (
     <div className="gallery">
+      <div className="g1">
+        <img alt="" src={require('./img/gal-1.jpeg')} />
+      </div>
+      <div className="g2">
+        <img alt="" src={require('./img/gal-2.jpeg')} />
+      </div>
+      <div className="g3">
+        <img alt="" src={require('./img/gal-3.jpeg')} />
+      </div>
+      <div className="g4">
+        <img alt="" src={require('./img/gal-4.jpeg')} />
+      </div>
+      <div className="g5">
+        <img alt="" src={require('./img/gal-5.jpeg')} />
+      </div>
+      <div className="g6">
+        <img alt="" src={require('./img/gal-6.jpeg')} />
+      </div>
+      <div className="g7">
+        <img alt="" src={require('./img/gal-7.jpeg')} />
+      </div>
+      <div className="g8">
+        <img alt="" src={require('./img/gal-8.jpeg')} />
+      </div>
+      <div className="g9">
+        <img alt="" src={require('./img/gal-9.jpeg')} />
+      </div>
+      <div className="g10">
+        <img alt="" src={require('./img/gal-10.jpeg')} />
+      </div>
+      <div className="g11">
+        <img alt="" src={require('./img/gal-11.jpeg')} />
+      </div>
+      <div className="g12">
+        <img alt="" src={require('./img/gal-12.jpeg')} />
+      </div>
+      <div className="g13">
+        <img alt="" src={require('./img/gal-13.jpeg')} />
+      </div>
+      <div className="g14">
+        <img alt="" src={require('./img/gal-14.jpeg')} />
+      </div>
     </div>
   );
 }
